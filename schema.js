d6dfea576eea72e55404c59c8fb102372fe4980d
CodeGuySean <seanbeanli@gmail.com>
2022-08-22 20:04:27 +0100

    show layout changed and validation enhancement

--- schema.js
+++ schema.js
@@ -26,10 +26,10 @@ const Joi = BaseJoi.extend(extension);
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required().escapeHTML(),
-        price: Joi.number().required().min(0),
+        price: Joi.number().required().min(0).max(100000),
         // image: Joi.string().required(),
         location: Joi.string().required().escapeHTML(),
-        description: Joi.string().required().escapeHTML()
+        description: Joi.string().required().escapeHTML().max(1000)
     }).required(),
     deleteImages: Joi.array()
 })
@@ -38,7 +38,7 @@ module.exports.campgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required().escapeHTML()
+        body: Joi.string().required().escapeHTML().min(1).max(100)
     }).required()
 })
     
