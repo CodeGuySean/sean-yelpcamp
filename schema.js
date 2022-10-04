48b53eeb1b8892fbab4b6a15b17588d514d36d0e
CodeGuySean <seanbeanli@gmail.com>
2022-10-05 00:05:11 +0100

    wording changed

--- schema.js
+++ schema.js
@@ -26,10 +26,10 @@ const Joi = BaseJoi.extend(extension);
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required().escapeHTML(),
-        price: Joi.number().required().min(0).max(100000),
+        price: Joi.number().required().min(0).max(1000),
         // image: Joi.string().required(),
         location: Joi.string().required().escapeHTML(),
-        description: Joi.string().required().escapeHTML().max(1000)
+        description: Joi.string().required().escapeHTML().max(500)
     }).required(),
     deleteImages: Joi.array()
 })
